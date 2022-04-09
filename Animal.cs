c428cafe4140217126f8b4d885cae67289be31a8
Samuel Bacellar <samuelbacellar1@gmail.com>
2022-04-09 16:10:15 -0300

    Material de programação do repositório

---
+++ Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimais
+{
+
+    public enum TipoAnimal {Cachorro, Gato, Peixe}
+
+    public class Animal
+    {
+        public Animal ()
+	    {
+            this.Nome = "";
+            this.Tipo = TipoAnimal.Peixe;
+	    }
+
+        public Animal (String nome, TipoAnimal tipo)
+	    {
+            this.Nome = nome;
+            this.Tipo = tipo;
+	    }
+
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private TipoAnimal tipo;
+
+        public TipoAnimal Tipo
+        {   //valores definidos pelo proprietário - Cachorro, Gato, Peixe
+            get { return tipo; }
+            set { tipo = value }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeAnimais
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a;
+            int tl = 0;
+            Animal[] animais = new Animal [20]; //array
+
+            int tlCachorro = 0, tlGato = 0, tlPeixe = 0; // contadores
+            int tipoAnimal = 0; //variavel
+
+            // leitura da quantidade de animais
+            try
+            {
+                Console.Write("Quantos animais deseja informar? (máximo 20) ");
+                tl = Convert.ToInt32(Console.ReadLine());
+                if (tl > 20) tl = 20;
+            }
+            catch
+            {
+                tl = 0;
+            }
+            for (int i = 0; i < tl; i++)
+			{
+                a = new Animal();
+                Console.WriteLine("Informe o nome do "+(i+1)+" animal: ");
+                a.Nome = Console.ReadLine();
+                Console.Write("Informe o tipo do "+(i+1)+"° animal (Cachorro - 0, Gato - 1 ou Peixe - 2): ");
+                try
+                {
+                    tipoAnimal = Convert.ToInt32(Console.ReadLine());
+
+                    //if((tipoAnimal < 0) && (tipoAnimal > 2)) tipoAnimal = 2;
+
+                    if(tipoAnimal < 0) tipoAnimal = 2;
+                    if(tipoAnimal > 2) tipoAnimal = 2;
+
+                }
+                catch
+                {
+                    tipoAnimal = 2;
+                }
+                a.Tipo = (TipoAnimal) tipoAnimal;
+                
+            // contando o tipo informado
+            if (a.Tipo == TipoAnimal.Cachorro) tlCachorro++;
+            if (a.Tipo == TipoAnimal.Gato) tlGato++;
+            if (a.Tipo == TipoAnimal.Peixe) tlPeixe++;
+            // armazenar o animal no vetor
+            animais[i] = a;
+
+			}
+            
+
+
+            //resultado
+            Console.WriteLine("Total de animais");
+            Console.WriteLine("Cachorros: " + tlCachorro + " \nGatos: " + tlGato + " \nPeixes: " + tlPeixe);
+            Console.ReadKey();
+        }
+    }
+}
